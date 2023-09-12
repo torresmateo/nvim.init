6072efed0515b9f7c5c1f5d0815fb5ffa62e4ceb
Mateo Torres <torresmateo@gmail.com>
2023-09-11 21:02:16 -0400

    changes to lsp

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -8,7 +8,6 @@ lsp.nvim_workspace()
 
 
 lsp.on_attach(function(client, bufnr)
-    print("help")
   local opts = {buffer = bufnr, remap = false}
 
   vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
