f315122d41dd115480a88944f415c3b94da9d20a
Mateo Torres <torresmateo@gmail.com>
2025-04-05 22:43:14 -0300

    added conf before computer wipe

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -7,6 +7,8 @@ require 'nvim-treesitter.configs'.setup {
         "markdown",
         "markdown_inline",
         "yaml",
+        -- "jinja",
+        "jinja_inline",
         "c",
         "templ",
         "lua",
@@ -35,7 +37,7 @@ require 'nvim-treesitter.configs'.setup {
         -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
         -- the name of the parser)
         -- list of language that will be disabled
-        -- disable = { "c", "rust" },
+        disable = { "dockerfile" },
         -- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
         -- disable = function(lang, buf)
         --     local max_filesize = 100 * 1024 -- 100 KB
--- lua/torresmateo/init.lua
+++ lua/torresmateo/init.lua
@@ -3,33 +3,35 @@ require("torresmateo.set")
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
     {
-        'nvim-telescope/telescope.nvim', tag = '0.1.5',
+        'nvim-telescope/telescope.nvim',
+        tag = '0.1.5',
         -- or                            , branch = '0.1.x',
-        dependencies = { {'nvim-lua/plenary.nvim'} }
+        dependencies = { { 'nvim-lua/plenary.nvim' } }
     },
     "ellisonleao/gruvbox.nvim",
-    {'nvim-treesitter/nvim-treesitter',
-    	build = ':TSUpdate'
+    {
+        'nvim-treesitter/nvim-treesitter',
+        build = ':TSUpdate'
     },
-    {'nvim-treesitter/playground'},
-    {'theprimeagen/harpoon'},
-    {'mbbill/undotree'},
-    {'windwp/nvim-projectconfig'},
-    {'tpope/vim-fugitive'},
-    {'lepture/vim-jinja'},
+    { 'nvim-treesitter/playground' },
+    { 'theprimeagen/harpoon' },
+    { 'mbbill/undotree' },
+    { 'windwp/nvim-projectconfig' },
+    { 'tpope/vim-fugitive' },
+    --{'lepture/vim-jinja'},
     {
         'nvim-lualine/lualine.nvim',
         dependencies = { 'nvim-tree/nvim-web-devicons', opt = true }
@@ -39,20 +41,19 @@ require("lazy").setup({
         branch = 'v2.x',
         dependencies = {
             -- LSP Support
-            {'neovim/nvim-lspconfig'},             -- Required
-            {                                      -- Optional
+            { 'neovim/nvim-lspconfig' }, -- Required
+            {                          -- Optional
                 'williamboman/mason.nvim',
                 run = function()
                     pcall(vim.cmd, 'MasonUpdate')
                 end,
             },
-            {'williamboman/mason-lspconfig.nvim'}, -- Optional
+            { 'williamboman/mason-lspconfig.nvim' }, -- Optional
 
             -- Autocompletion
-            {'hrsh7th/nvim-cmp'},     -- Required
-            {'hrsh7th/cmp-nvim-lsp'}, -- Required
-            {'L3MON4D3/LuaSnip'},     -- Required
+            { 'hrsh7th/nvim-cmp' },   -- Required
+            { 'hrsh7th/cmp-nvim-lsp' }, -- Required
+            { 'L3MON4D3/LuaSnip' },   -- Required
         }
     }
 })
-
